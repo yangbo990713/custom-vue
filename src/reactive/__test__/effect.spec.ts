b27a841a8980e101b3c01525a568a2ed1e42e612
杨博 <1107653974@qq.com>
2022-07-18 16:52:06 +0800

    feat(reactive): 实现effect的scheduler功能

--- src/reactive/__test__/effect.spec.ts
+++ src/reactive/__test__/effect.spec.ts
@@ -2,31 +2,64 @@ import {effect} from "../effect";
 import {reactive} from "../reactive";
 
 describe('effect', function () {
-  it('happy path', function () {
-    const person = {
-      name: '小明',
-      age: 12
-    }
+  it('base', function () {
+
+    const person = {name: '小明', age: 12}
     const proxy = reactive(person)
 
     let newAge
-
     // 调用 effect 传递一个回调函数
-    const runner = effect(() => {
+    effect(() => {
       // 由于"读"到了age属性,会触发"proxy"的依赖收集 -> reactive.ts
       newAge = proxy.age + 1
       return 'runner'
     })
-    // 断言 newAge 为 11 因为effect会立马执行一次回调
+    // 断言 newAge 为 13 因为effect会立马执行一次回调
     expect(newAge).toBe(13)
 
     // 修改age,newAge应该也要更新
     proxy.age = 11
     expect(newAge).toBe(12)
 
-    // 调用runner 执行一次回调 并返回 回调方法 的返回值
+
+  });
+
+  it('runner', function () {
+    let num = 1
+    const runner = effect(() => {
+      num++
+      return 'runner'
+    })
+    // 调用 runner 执行一次回调 并返回 回调方法 的返回值
     let res = runner()
     expect(res).toBe('runner')
-    expect(newAge).toBe(12)
+    // effect会自动调用一次回调 runner也会调用一次
+    expect(num).toBe(3)
   });
+
+  it('scheduler', () => {
+    // 1.effect调用时会执行一次fn
+    // 2.依赖变化时会执行scheduler,而不是fn
+    // 3.调用effect返回的run方法才会执行fn
+    let dummy
+    let run: any
+    const scheduler = jest.fn(() => {
+      run = runner
+    })
+    const obj = reactive({foo: 1})
+    const runner = effect(() => {
+      dummy = obj.foo
+    }, {scheduler})
+    // effect执行时不会调用scheduler
+    expect(scheduler).not.toHaveBeenCalled()
+    expect(dummy).toBe(1)
+    // 触发更新时 scheduler 会被调用一次
+    obj.foo++
+    expect(scheduler).toHaveBeenCalledTimes(1)
+    // 且fn回调不会被调用
+    expect(dummy).toBe(1)
+    // 调用effect返回的run方法时,才会调用fn回调
+    run()
+    expect(dummy).toBe(2)
+  })
 });
--- src/reactive/effect.ts
+++ src/reactive/effect.ts
@@ -1,9 +1,9 @@
-
 class ReactiveEffect {
   private readonly fn: Function;
 
-  constructor(fn: Function) {
+  constructor(fn: Function, public scheduler?: Function | undefined) {
     this.fn = fn
+    this.scheduler = scheduler
   }
 
   run() {
@@ -14,9 +14,9 @@ class ReactiveEffect {
 // 当前的更新函数
 let activeEffect: ReactiveEffect
 
-export function effect(fn: Function) {
+export function effect(fn: Function, options: any = {}) {
   // 实例化一个ReactiveEffect类,保存回调函数
-  activeEffect = new ReactiveEffect(fn)
+  activeEffect = new ReactiveEffect(fn, options.scheduler)
   // 立刻执行一次
   activeEffect.run()
   return activeEffect.run.bind(activeEffect)
@@ -25,31 +25,48 @@ export function effect(fn: Function) {
 // 所有的被收集的数据,以及它们的更新方法
 const depMap = new Map()
 
+/**
+ * 收集依赖
+ * @param target 目标对象
+ * @param key 目标key
+ */
 export function track(target: object, key: string | symbol) {
+  // target -> key -> deps
+
   // 从depMap中取出target对应的数据
   let targetMap = depMap.get(target)
   if (!targetMap) {
     targetMap = new Map()
+    depMap.set(target, targetMap)
   }
+
   // 取出target中key对应的更新方法
   let deps = targetMap.get(key)
   if (!deps) {
     deps = new Set()
+    targetMap.set(key, deps)
   }
-  // 把当前更新方法添加到 key 对应的 更新方法数组 中
+
+  // 把当前更新方法添加到 key 对应的 依赖数组 中
   deps.add(activeEffect)
-  // 重设 key 对应的 更新方法数组
-  targetMap.set(key, deps)
-  // 重设 target 对应的 数据
-  depMap.set(target, targetMap)
+
 }
 
+/**
+ * 通知更新方法
+ * @param target 目标对象
+ * @param key 目标key
+ */
 export function notify(target: object, key: string | symbol) {
   // 以下两步 为 取出key对应的 更新方法数组
   let targetMap = depMap.get(target)
   let deps = targetMap.get(key)
   // 挨个调用更新方法
-  Array.from(deps).forEach((item: any) => {
-    item.run()
-  })
+  for (const depItem of deps) {
+    if (depItem.scheduler) {
+      depItem.scheduler()
+    } else {
+      depItem.run()
+    }
+  }
 }
